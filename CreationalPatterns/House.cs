e7d6a62c096e4f60540516854defd0aa01526cc7
Rostyslav Neborakovskyi <rostislav.neborakovskiy@hotmail.com>
2023-11-30 22:30:31 +0200

    Builder

---
+++ CreationalPatterns/House.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPatterns
+{
+    public class House
+    {
+        public string Address { get; set; }
+        public bool HasSwimmingPool { get; set; }
+        public bool HasGarden { get; set; }
+        public bool HasGarage { get; set; }
+        public bool HasFancyStatues { get; set; }
+
+        public House(string address)
+        {
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            var isSwimmingPool = HasSwimmingPool ? "yes" : "no";
+            var isFancyStatues = HasFancyStatues ? "yes" : "no";
+            var isGarden = HasGarden ? "yes" : "no";
+            var isGarage = HasGarage ? "yes" : "no";
+
+            var result = $"House Address: {Address} \n\r" +
+                                "House facilities: \n\r" +
+                                $"\tSwimming Pool: \t\t{isSwimmingPool} \n\r" +
+                                $"\tFancy Statues: \t\t{isFancyStatues} \n\r" +
+                                $"\tGarden: \t\t{isGarden} \n\r" +
+                                $"\tGarage: \t\t{isGarage} \n\r" +
+                                "------------------------------------------- \n\r";
+            return result;
+        }
+    }
+
+    public interface IHouseBuilder
+    {
+        IHouseBuilder WithSwimmingPool();
+        IHouseBuilder WithGarden();
+        IHouseBuilder WithGarage();
+        IHouseBuilder WithFancyStatues();
+        House GetHouse();
+    }
+
+    public class HouseBuilder : IHouseBuilder
+    {
+        private readonly House _house;
+
+        public HouseBuilder(string address) => _house = new House(address);
+        protected HouseBuilder(House house) => _house = house;
+
+        public IHouseBuilder WithSwimmingPool()
+        {
+            _house.HasSwimmingPool = true;
+            return this;
+        }
+
+        public IHouseBuilder WithGarden()
+        {
+            _house.HasGarden = true;
+            return this;
+        }
+
+        public IHouseBuilder WithGarage()
+        {
+            _house.HasGarage = true;
+            return this;
+        }
+
+        public IHouseBuilder WithFancyStatues()
+        {
+            _house.HasFancyStatues = true;
+            return this;
+        }
+
+        public House GetHouse() => _house;
+    }
+
+    public class HouseDirector
+    {
+        private readonly IHouseBuilder _houseBuilder;
+
+        public HouseDirector(IHouseBuilder houseBuilder) => _houseBuilder = houseBuilder;
+
+        public House AddGarage()
+        {
+            _houseBuilder.WithGarage();
+            return _houseBuilder.GetHouse();
+        }
+        public House AddGarden()
+        {
+            _houseBuilder.WithGarden();
+            return _houseBuilder.GetHouse();
+        }
+        public House AddSwimmingPool()
+        {
+            _houseBuilder.WithSwimmingPool();
+            return _houseBuilder.GetHouse();
+        }
+        public House AddFancyStatues()
+        {
+            _houseBuilder.WithFancyStatues();
+            return _houseBuilder.GetHouse();
+        }
+    }
+}
---
+++ CreationalPatterns/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace CreationalPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var houseBuilder = new HouseBuilder("st.Main 1");
+            var houseDirector = new HouseDirector(houseBuilder);
+
+            var house = houseBuilder.GetHouse();
+            Console.WriteLine(house.ToString());
+            
+            house = houseDirector.AddGarage();
+            Console.WriteLine(house.ToString());
+
+            house = houseDirector.AddFancyStatues();
+            Console.WriteLine(house.ToString());
+            
+            var houseWithSwimmingPool = new HouseBuilder("st.Main 2").WithSwimmingPool().GetHouse();
+            Console.WriteLine(houseWithSwimmingPool.ToString());
+
+            var houseWithFancyStatues = new HouseBuilder("st.Main 3").WithFancyStatues().GetHouse();
+            Console.WriteLine(houseWithFancyStatues.ToString());
+
+            var houseWithGarage = new HouseBuilder("st.Main 4").WithGarage().GetHouse();
+            Console.WriteLine(houseWithGarage.ToString());
+
+            var houseWithGarden = new HouseBuilder("st.Main 5").WithGarden().GetHouse();
+            Console.WriteLine(houseWithGarden.ToString());
+
+        }
+    }
+}
+/*
+House Address: st.Main 1
+House facilities:
+        Swimming Pool:          no
+        Fancy Statues:          no
+        Garden:                 no
+        Garage:                 no
+-------------------------------------------
+
+House Address: st.Main 1
+House facilities:
+        Swimming Pool:          no
+        Fancy Statues:          no
+        Garden:                 no
+        Garage:                 yes
+-------------------------------------------
+
+House Address: st.Main 1
+House facilities:
+        Swimming Pool:          no
+        Fancy Statues:          yes
+        Garden:                 no
+        Garage:                 yes
+-------------------------------------------
+
+House Address: st.Main 2
+House facilities:
+        Swimming Pool:          yes
+        Fancy Statues:          no
+        Garden:                 no
+        Garage:                 no
+-------------------------------------------
+
+House Address: st.Main 3
+House facilities:
+        Swimming Pool:          no
+        Fancy Statues:          yes
+        Garden:                 no
+        Garage:                 no
+-------------------------------------------
+
+House Address: st.Main 4
+House facilities:
+        Swimming Pool:          no
+        Fancy Statues:          no
+        Garden:                 no
+        Garage:                 yes
+-------------------------------------------
+
+House Address: st.Main 5
+House facilities:
+        Swimming Pool:          no
+        Fancy Statues:          no
+        Garden:                 yes
+        Garage:                 no
+-------------------------------------------
+
+ */
